6f26bd39047528f44fbd861a56eae0a2e64364db
LI Yu <liyu@clustertech.com>
2014-02-10 14:10:16 +0800

    intro unit testing with mocha (coverage by blanket), and workflow.js is now 100% covered.

---
+++ test/workflow.js
@@ -0,0 +1,140 @@
+var util = require('util');
+var assert = require('assert');
+
+var WorkflowMgr = require('../lib/workflow');
+
+var _output = '';
+
+function output() {
+  _output += util.format.apply(null, arguments);
+}
+
+function getMockOK(callback) {
+  return function(local) {
+    callback(local);
+  };
+}
+
+function getMockErr(callback) {
+  return function(local) {
+    callback(local);
+  };
+}
+
+var _jury = 100;
+function judge(callback) {
+  var r = _jury;
+  if (r > 50) {
+    callback('D');
+  } else if (r < 10) {
+    callback('ERR');
+  } else {
+    callback('C');
+  }
+}
+
+function getAWorkflow(jury) {
+  _output = '';
+  _jury = jury;
+  var w = WorkflowMgr.newWorkflow({});
+  w.def({
+    'A': function() {
+      output('A');
+      return 'B';
+    },
+
+    'B': function() {
+      var self = this;
+      output('B');
+      judge(function(nextStep) {
+          self.goto(nextStep);
+        });
+    },
+
+    'C': function() {
+      output('C');
+      this.local.hello = 'world!';
+      return 'END';
+    },
+
+    'D': function() {
+      output('D');
+      this.local.hello = 'rask!';
+      return 'END';
+    }
+  });
+  return w;
+}
+
+describe('workflow', function() {
+  describe('testEND1', function() {
+    it('should finish without error.', function(done) {
+      var w = getAWorkflow(30);
+      w.start(getMockOK(function(local) {
+        if (_output === 'ABC' && local.hello === 'world!') {
+          done();
+        } else {
+          throw util.format('wrong results: %s, %j', _output, local);
+        }
+      }));
+    });
+  });
+
+  describe('testEND2', function() {
+    it('should finish without error.', function(done) {
+      var w = getAWorkflow(70);
+      w.start(getMockOK(function(local) {
+        if (_output === 'ABD' && local.hello === 'rask!') {
+          done();
+        } else {
+          throw util.format('wrong results: %s, %j', _output, local);
+        }
+      }));
+    });
+  });
+
+  describe('testERR', function() {
+    it('should finish with calling err handler.', function(done) {
+      var w = getAWorkflow(7);
+      w.start(null, getMockErr(function(local) {
+        done();
+      }));
+    });
+  });
+
+  describe('testDeregister', function() {
+    it('should finish without error.', function(done) {
+      var w = getAWorkflow(7);
+      w.start(null, getMockErr(function(local) {
+        WorkflowMgr.deregister(w.meta._uuid);
+        done();
+      }));
+    })
+  });
+
+  describe('testNULL', function() {
+    it('should finish without error.', function(done) {
+      var w = WorkflowMgr.newWorkflow();
+      w.def({});
+      w.start(getMockOK(function(local) {
+        done();
+      }));
+    });
+  });
+
+  describe('testWrongState', function() {
+    it('should finish without error.', function(done) {
+      var w = WorkflowMgr.newWorkflow();
+      w.def({
+        'A': function() {
+          return 'B';
+        }
+      });
+      w.on('wrong_state', function(err) {
+        // got the wrong state event
+        done();
+      });
+      w.start();
+    });
+  });
+});
